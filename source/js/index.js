723ec47fac9685ae4c90f55e5df0e72cd06d5cd3
Vladimir-Bumagin <vl.bumagin@yandex.ru>
2022-12-04 16:43:27 +0300

    Добавляет стили мобильной версии хедера

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,25 @@
 const mainNavToggle = document.querySelector('.main-nav__toggle');
+const popUpMenu = document.querySelector('.pop-up-menu');
+const popUpCloseButton = document.querySelector('.pop-up-menu__button-close');
+
+const openPopUp = function() {
+  mainNavToggle.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popUpMenu.classList.toggle('pop-up-menu--is-open');
+  mainNavToggle.classList.toggle('visually-hidden');
+  popUpCloseButton.classList.toggle('visually-hidden');
+  });
+}
+
+const closePopUp = function () {
+  popUpCloseButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    popUpMenu.classList.toggle('pop-up-menu--is-open');
+    mainNavToggle.classList.toggle('visually-hidden');
+    popUpCloseButton.classList.toggle('visually-hidden');
+    });
+}
+
+openPopUp();
+closePopUp();
+
