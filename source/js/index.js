df48513cb0e7053054e6f2846555b23151eb5244
Vladimir-Bumagin <vl.bumagin@yandex.ru>
2022-12-15 12:36:01 +0300

    Добавляет состояния меню хедера без и с подключённым js

--- source/js/index.js
+++ source/js/index.js
@@ -2,6 +2,14 @@ const mainNavToggle = document.querySelector('.main-nav__toggle');
 const popUpMenu = document.querySelector('.pop-up-menu');
 const popUpCloseButton = document.querySelector('.pop-up-menu__button-close');
 
+if (popUpMenu.classList.contains('pop-up-menu--no-js')) {
+  popUpMenu.classList.remove('pop-up-menu--no-js')
+}
+
+if (mainNavToggle.classList.contains('main-nav__toggle--no-js')) {
+  mainNavToggle.classList.remove('main-nav__toggle--no-js')
+}
+
 const openPopUp = function() {
   mainNavToggle.addEventListener('click', (evt) => {
   evt.preventDefault();
