a54bbd78e341b371c860e8b089a52841e4e4742d
Vladimir-Bumagin <vl.bumagin@yandex.ru>
2023-01-20 13:18:47 +0300

    Добавляет критической функциональности карте на главной странице при отключенном js

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,8 @@
 const mainNavToggle = document.querySelector('.main-nav__toggle');
 const popUpMenu = document.querySelector('.pop-up-menu');
 const popUpCloseButton = document.querySelector('.pop-up-menu__button-close');
+const mapLink = document.querySelector('.map__link');
+const mapFrame = document.querySelector('.map__frame');
 
 if (popUpMenu.classList.contains('pop-up-menu--no-js')) {
   popUpMenu.classList.remove('pop-up-menu--no-js')
@@ -8,6 +10,14 @@ if (popUpMenu.classList.contains('pop-up-menu--no-js')) {
 
 if (mainNavToggle.classList.contains('main-nav__toggle--no-js')) {
   mainNavToggle.classList.remove('main-nav__toggle--no-js')
+}
+
+if (mapLink.classList.contains('map__link--no-js')) {
+  mapLink.classList.remove('map__link--no-js')
+}
+
+if (mapFrame.classList.contains('map__frame--no-js')) {
+  mapFrame.classList.remove('map__frame--no-js')
 }
 
 const openPopUp = function() {
